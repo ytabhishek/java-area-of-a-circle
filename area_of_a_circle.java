34cec333cc23318242bcb230731333f8e51c1701
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2024-08-06 14:23:38 +0530

    Add files via upload

---
+++ area_of_a_circle.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class area_of_a_circle {
+    public static void main(String[] args) {
+        double r = 5;
+        double pai = 3.1415;
+        double area = pai * r * r;
+        System.out.print("The area of circle is : " + area);
+
+    }
+}
